906288e37aa8c2119eacab35c099deefd2496e84
usk001 <usk.ktkt@gmail.com>
2024-07-26 07:14:26 +0900

    Updates

---
+++ precache-manifest.000b6b1f7fee7ecc1bb7f3abe48b93ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3889355a2a5442ccf761675d4de12a9e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "8d7fd89a8e7a0d12c3ec",
+    "url": "/my-app/static/css/main.132c13fb.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/my-app/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/my-app/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "8d7fd89a8e7a0d12c3ec",
+    "url": "/my-app/static/js/main.31dd88cf.chunk.js"
+  },
+  {
+    "revision": "330a69b442416f742e8c",
+    "url": "/my-app/static/js/runtime~main.148f857f.js"
+  }
+]);
