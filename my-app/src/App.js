76874f402ce9d2c5f50df3c6ed1884717a5d5b16
usk001 <usk.ktkt@gmail.com>
2024-07-26 06:32:23 +0900

    movie

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+// src/App.js
+
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video className="background-video" autoPlay loop muted playsInline>
+        <source src="/my-app/top.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 }
